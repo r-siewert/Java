d1cd67fe5e6bfa10dd71aafeea9424d5ce169e9f
r-siewert <reinhard.siewert@web.de>
2026-09-13 20:09:00 +0200

    JavaWeek5praccIncomplete

---
+++ praccen/src/prac1.java
@@ -0,0 +1,31 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class prac1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] zahlenanalyse = new int[5];
+        int summe = 0;
+
+        System.out.println("Bitte geben Sie 5 ganze Zahlen ein: ");
+
+        for (int i = 0; i < 5; i++) {
+            int eingabe = scanner.nextInt();
+            zahlenanalyse[i] = eingabe;
+            summe += zahlenanalyse[i];
+        }
+
+        for (int j = 0; j < 5; j++) {
+
+        }
+
+        int maximum = zahlenanalyse[0];
+        int minimum = zahlenanalyse[0];
+
+        System.out.println(Arrays.toString(zahlenanalyse));
+        System.out.println(summe);
+    }
+
+}
