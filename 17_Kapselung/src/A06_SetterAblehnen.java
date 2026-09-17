e2d59ac508ab2771d14005d73ff92370bf8b8277
r-siewert <reinhard.siewert@web.de>
2026-09-17 12:59:10 +0200

    new

---
+++ 17_Kapselung/src/A06_SetterAblehnen.java
@@ -0,0 +1,30 @@
+class Punktestand {
+    private int punkte;
+
+    public void setPunkte(int ppunkte) {
+        if (ppunkte > 0) {
+            this.punkte = ppunkte;
+        } else {
+            this.punkte = 0;
+        }
+    }
+
+    public int getPunkte() {
+        return punkte;
+    }
+}
+
+public class A06_SetterAblehnen {
+    public static void main(String[] args) {
+        Punktestand punktestand = new Punktestand();
+
+        punktestand.setPunkte(50);
+
+        System.out.println("Punktestand: " + punktestand.getPunkte());
+
+        punktestand.setPunkte(-50);
+
+        System.out.println("Punktestand: " + punktestand.getPunkte());
+    }
+
+}
