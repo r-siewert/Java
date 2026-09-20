7557053c1c410885d18701f48d236960f953857f
r-siewert <reinhard.siewert@web.de>
2026-09-20 14:56:35 +0200

    update

--- 16_Konstruktoren_und_This/src/praccA02_Konstruktorablauf.java
+++ 16_Konstruktoren_und_This/src/praccA02_Konstruktorablauf.java
@@ -7,7 +7,8 @@ class Bestellung {
 
 public class praccA02_Konstruktorablauf {
     public static void main(String[] args) {
-        Bestellung bestellung = new Bestellung();
+        // Bestellung bestellung = new Bestellung(); Auskommentiert, damit es nicht gelb
+        // ist!
 
         System.out.println("Main laeuft.");
     }
