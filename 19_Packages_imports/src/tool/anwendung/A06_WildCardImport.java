72272008f45d7822f6bb4e63f74d114004b2cc76
r-siewert <reinhard.siewert@web.de>
2026-09-22 11:35:24 +0200

    update

---
+++ 19_Packages_imports/src/tool/anwendung/A06_WildCardImport.java
@@ -0,0 +1,23 @@
+package tool.anwendung;
+
+// der Wildcard.import stellt alle öffentlich zugängliche Klassen 
+// und Interfaces aus dem Paket java.util. zur Verfügung
+import java.util.*; // * -> Wildcard
+
+public class A06_WildCardImport {
+    public static void main(String[] args) {
+
+        ArrayList<String> namen = new ArrayList<>();
+        HashSet<Integer> punkte = new HashSet<>();
+
+        namen.add("Peter");
+        namen.add("Pan");
+
+        punkte.add(10);
+        punkte.add(10);
+        punkte.add(11);
+
+        System.out.println("Name: " + namen.size());
+        System.out.println("Punkte: " + namen.size());
+    }
+}
