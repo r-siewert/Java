e43109a115a290efd36df6c960e045967abe9b6e
r-siewert <reinhard.siewert@web.de>
2026-09-22 11:43:06 +0200

    update

---
+++ 19_Packages_imports/src/tool/anwendung/A07_StatischerImport.java
@@ -0,0 +1,16 @@
+package tool.anwendung;
+
+import static java.lang.Math.PI;
+
+import static java.lang.Math.round;
+
+public class A07_StatischerImport {
+
+    public static void main(String[] args) {
+
+        double umfang = 2 * PI * 3;
+
+        System.out.println("Umfang: " + round(umfang));
+
+    }
+}
